9a92df740e038c9fbf706cfc13564d2ed4223369
JaluGJ <gabriel.i.jalil@gmail.com>
2023-01-04 18:22:11 -0300

    ADD: server starts

---
+++ backend/index.ts
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const {db} = require("./src/db.ts")
+const app = require("./src/app.ts")
+const {PORT} = process.env
+
+db.initialize().then(()=>{
+  console.log("La db fue conectada exitosamente")
+  app.listen(PORT, () => {
+    console.log(`Server levantado en puerto ${PORT}`)
+  })
+}).catch((error) => {
+  console.error("Error durante la conexión", error)
+})
---
+++ backend/src/db.ts
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const { DataSource } = require("typeorm")
+const { PG_DATABASE, PG_USER, PG_PORT, PG_DIALECT, PG_PASSWORD, PG_HOST } = process.env
+
+
+const db = new DataSource({
+  type: PG_DIALECT,
+  host: PG_HOST,
+  port: PG_PORT,
+  username:PG_USER,
+  password:PG_PASSWORD,
+  database:PG_DATABASE,
+  dropSchema: false,
+})
+
+module.exports = {
+  db
+}
